765e1adb6e7a5f5c3f0f25219e1086960e4c7e79
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-11-06 19:28:23 +0100

    Video found, link needs to be searched

--- MusicCrawler.py
+++ MusicCrawler.py
@@ -10,3 +10,9 @@ search_term = input("Please type in the song name: ")
 search_url = base_url + "results?search_query=" + search_term
 print(search_url)
 
+
+def get_music_link():
+    r = requests.get(search_url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    link = soup.find("//*[@id="items"]/ytd-video-renderer[1]")
+    
