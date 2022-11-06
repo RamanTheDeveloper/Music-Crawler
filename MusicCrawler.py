1b86cb2cac47d6e1c126bbf000294e3c3f5e4f6e
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-11-06 19:02:09 +0100

    Function created for search

---
+++ MusicCrawler.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+#Youtube URL
+base_url = "ww.youtube.com"
+
+def search():
+    search_term = input("Please type in the song name: ")
+    
+
+
+def get_music_links():
+    r = requests.get()
