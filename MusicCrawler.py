6e2e84651dc7f26cffbb992e06ac28b41c1d60ac
RamanTheDeveloper <ramandeep-singh25@outlook.com>
2022-11-06 19:20:39 +0100

    Url to fetch searched term

--- MusicCrawler.py
+++ MusicCrawler.py
@@ -3,12 +3,10 @@ from bs4 import BeautifulSoup
 
 
 #Youtube URL
-base_url = "ww.youtube.com"
+base_url = "https://www.youtube.com/"
+converter_url = "https://ytmp3.nu/"
 
-def search():
-    search_term = input("Please type in the song name: ")
-    
+search_term = input("Please type in the song name: ")
+search_url = base_url + "results?search_query=" + search_term
+print(search_url)
 
-
-def get_music_links():
-    r = requests.get()
